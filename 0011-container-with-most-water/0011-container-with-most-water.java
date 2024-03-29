cf8b5e39d4e86fe203ee4de1c3cf4a94f6ce1006
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-03-29 18:22:43 +0530

    Time: 5 ms (71.97%), Space: 57.8 MB (58.55%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] arr) {
+        int maxwaterarea=0 ; 
+        int start = 0 ; 
+        int lastpoint = arr.length-1;
+        while(start<=lastpoint){
+            int min = Math.min(arr[start],arr[lastpoint]);
+            int multiplier=lastpoint-start;
+            int currentarea=min*multiplier;
+           if(maxwaterarea<currentarea){
+            maxwaterarea=currentarea;
+           }
+           if(arr[start]<arr[lastpoint]){
+            start++;
+           }
+           else if(arr[start]>=arr[lastpoint]){
+            lastpoint--;
+           }
+        }
+        return maxwaterarea;
+    }
+}
