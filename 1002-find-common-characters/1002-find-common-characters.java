c9790f41c390d7108cefc36a4269c74c43f83fa8
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-04-03 10:44:54 +0530

    Time: 24 ms (10.77%), Space: 44.7 MB (23.3%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -3,33 +3,32 @@ class Solution {
         String s = words[0];
         Map<Character, Integer> firststringmap = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-                firststringmap.put(s.charAt(i), firststringmap.getOrDefault(s.charAt(i), 0)+1);
+            firststringmap.put(s.charAt(i), firststringmap.getOrDefault(s.charAt(i), 0) + 1);
         }
-       
-        for(int i = 1 ; i < words.length;i++){
-            Map<Character,Integer> otherstringmap = new HashMap<>();
-            for( int j =0 ; j<words[i].length();j++){
-                otherstringmap.put(words[i].charAt(j), otherstringmap.getOrDefault(words[i].charAt(j),0)+1);
+
+        for (int i = 1; i < words.length; i++) {
+            Map<Character, Integer> otherstringmap = new HashMap<>();
+            for (int j = 0; j < words[i].length(); j++) {
+                otherstringmap.put(words[i].charAt(j), otherstringmap.getOrDefault(words[i].charAt(j), 0) + 1);
             }
-            for(Character key: firststringmap.keySet()){
-                if((otherstringmap.containsKey(key) && firststringmap.get(key)>0)){
-                firststringmap.put(key,Math.min(firststringmap.get(key),otherstringmap.get(key)));
+            for (Character key : firststringmap.keySet()) {
+                if ((otherstringmap.containsKey(key) && firststringmap.get(key) > 0)) {
+                    firststringmap.put(key, Math.min(firststringmap.get(key), otherstringmap.get(key)));
                 }
-            
-            else{
-                firststringmap.put(key,0);
+
+                else {
+                    firststringmap.put(key, 0);
+                }
             }
         }
-    }
-    System.out.println(firststringmap);
-    List<String> list = new ArrayList<>();
-        for(Character key:firststringmap.keySet()){
-            if(firststringmap.get(key)>0)
-            {
-                for(int i=0;i<firststringmap.get(key);i++)
-                list.add(key+"");
+        System.out.println(firststringmap);
+        List<String> list = new ArrayList<>();
+        for (Character key : firststringmap.keySet()) {
+            if (firststringmap.get(key) > 0) {
+                for (int i = 0; i < firststringmap.get(key); i++)
+                    list.add(key + "");
             }
         }
         return list;
     }
-    }
+}
