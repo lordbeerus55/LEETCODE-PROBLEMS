730b150f8db00723620e04ecbdbe84893005f3ef
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-06-23 14:14:23 +0530

    Time: 2 ms (83.41%), Space: 45.8 MB (76.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,14 +2,12 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int notzeroelement = 0 ; 
         for(int i = 0 ; i <nums.length;i++){
-                    if(nums[i]!=0){
-                        int temp = nums[i];
-                        nums[i]=nums[notzeroelement];
-                        nums[notzeroelement]=temp;
-                        notzeroelement++;
-                        
-                    }
-
-                 }
+            if(nums[i]!=0){
+                int temp = nums[notzeroelement];
+                nums[notzeroelement]=nums[i];
+                nums[i]=temp;
+                notzeroelement++;
+            }
+        }
     }
 }
