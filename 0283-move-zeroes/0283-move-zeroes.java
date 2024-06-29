abc8ea503e72d7a1046750923ec2751afdb156e5
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-06-29 19:10:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int notzeroelement = 0 ; 
-        for(int i = 0 ; i <nums.length;i++){
+        for(int i = 1 ; i <nums.length;i++){
             if(nums[i]!=0){
                 int temp = nums[notzeroelement];
                 nums[notzeroelement]=nums[i];
