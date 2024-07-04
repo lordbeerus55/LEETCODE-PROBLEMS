2b82d35c450ae78a560aee7814c0d05a93765d78
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-07-05 01:38:49 +0530

    Time: 0 ms (100%), Space: 45.3 MB (98.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int[] answer = {-1,-1};
+        int start=binarysearch(nums,true,target);
+        int end =binarysearch(nums,false,target);
+        answer[0]=start;
+        answer[1]=end;
+       return answer;   
        
-        int [] ans = {-1,-1};
-       int start = search(nums, target, true);
-        int end=search(nums, target, false);
-        ans[0]=start;
-        ans[1]=end;
-
-        return ans;
-        
     }
-    static int search(int[] nums , int target , boolean findStartIndex){
-
+    public int binarysearch(int[] nums, boolean findstartornot,int target){
+        int start =0;
+        int end = nums.length-1;
         int ans=-1;
-        int start = 0;
-        int end =nums.length-1;
         while(start<=end){
-            int mid = start +(end-start)/2;
-            if(target<nums[mid]){
-                end=mid-1;
-            }
-            else if(target>nums[mid]){
-                start = mid+1;
+            int mid = start+(end-start)/2;
 
+            if(nums[mid]>target){
+                end=mid-1;
+                
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
             }
             else{
                 ans=mid;
-                if(findStartIndex){
-                    end=mid-1;
+                if(findstartornot==true){
+                                       end=mid-1;
+
                 }
                 else{
-                    start=mid+1;
+                     start=mid+1;
                 }
             }
-           
         }
         return ans;
-        
     }
 }
