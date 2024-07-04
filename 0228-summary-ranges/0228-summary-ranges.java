690ac7459a74c65863b67731b1cce30de3ac66bf
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-07-04 23:22:50 +0530

    Time: 6 ms (38.88%), Space: 41.7 MB (23.74%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,36 +1,26 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> list= new ArrayList<String>();
-
-        StringBuilder sb= new StringBuilder();
-        for(int i= 0; i< nums.length; i++){
-            int prev_diff= 0;
-            int next_diff= 0;
-            if(sb.length()== 0)
-                sb.append(String.valueOf(nums[i]));
-
-            // check previous difference
-                if(i> 0)
-                    prev_diff= nums[i]- nums[i-1];
-
-            // check next difference
-                if(i< nums.length-1)
-                    next_diff= nums[i+1]- nums[i];
-
-            if(prev_diff== 1 && next_diff!= 1){
-                sb.append("->"+ nums[i]);
-                list.add(sb.toString());
-                sb.setLength(0);
+      List<String> result = new ArrayList<>();
+        int start =0 ; 
+        int end = 0 ;
+        int i =0 ;
+        int n = nums.length;
+        while(i<n){
+            if(i+1<n&&nums[i+1]-nums[i]==1){
+                end++;
             }
-
-            if(prev_diff!= 1 && next_diff!= 1){
-                list.add(sb.toString());
-                sb.setLength(0);
+            else{
+                if(start==end){
+                    result.add(String.valueOf(nums[start]));
+                }
+                else{
+                    result.add(nums[start]+"->"+nums[end]);
+                }
+                end++;
+                start=end;
             }
-
-
+            i++;
         }
-
-        return list;        
+        return result;
     }
 }
