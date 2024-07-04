8f1295ab0b8b9efa3947934704b16971874d6ca1
lordbeerus55 <86998878+lordbeerus55@users.noreply.github.com>
2024-07-05 00:15:21 +0530

    Time: 31 ms (60.55%), Space: 51.5 MB (56.01%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,44 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            // skip i duplicates for long af testcases that leetcode has
-            if (i > 0 && nums[i] == nums[i - 1]) {
+       List<List<Integer>> finalanswer= new ArrayList<>();
+       for(int i=0;i<nums.length-2;i++){
+
+        if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+         int j = i+1;
+         int k = nums.length-1;
+         while(j<k){
+            int sum = nums[i]+nums[j]+nums[k];
 
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            if(sum==0){
+                finalanswer.add(Arrays.asList(nums[i],nums[j],nums[k]));
+            
 
-                if (sum == 0) {
-                    //found an answer then add
-                    answer.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    // skip j duplicates
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-                    // skip k duplicates
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-
-                    }
-                    j++;
-                    k--;
-                }
-                // if sum is more than 0 then only increase j
-                else if (sum < 0) {
-                    j++;
-
-                }
-                //if sum more than 0 then only decrease k ; 
-                 else {
-                    k--;
-                }
+            while(j<k&&nums[j]==nums[j+1]){
+                j++;
             }
-        }
-        return answer;
+            while(j<k&&nums[k]==nums[k-1]){
+             k--;
+
+
+            }
+            j++;
+            k--;
+
+            }
+            else if(sum<0){
+                j++;
+            }
+            else{
+                k--;
+            }
+
+
+         }
+       }
+       return finalanswer;
     }
 }
